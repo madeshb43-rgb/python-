bee895b2e03c507a1d583f50875e1f183a9ebd25
madesh.b <madeshb43@gmail.com>
2026-09-12 20:51:06 +0530

    python basic
    python basic identifiers variables and data types

---
+++ python basic.py
@@ -0,0 +1,53 @@
+#Python Programming
+#1. Python — Definition
+#Python is a high-level, interpreted, general-purpose programming language. 
+#It is easy to learn because its syntax is simple and readable
+print("Hello, World!")
+#Identifier in Python
+#Definition:An identifier is the name given to elements in a Python program, such as:
+#Variables,Functions,Classes,Objects,Modules
+student_name = "Madesh"
+student_age = 21
+
+print(student_name)
+print(student_age)
+
+#Variable in Python
+#Definition:A variable is a name used to store a value in a Python program.
+#Python does not require you to specify the variable's data type when creating it.
+##variable_name = value
+name = "Madesh"
+age = 21
+height = 5.7
+
+print(name)
+print(age)
+print(height)
+#Data Types in Python
+#Definition
+#A data type defines the type of value that a variable can store.
+#Integer (int)
+#Definition:An integer is a whole number without a decimal point.
+age = 21
+marks = 85
+print(age)
+print(marks)
+print(type(age))
+#Float (float)
+#Definition:A float is a number that contains a decimal point.
+height = 5.7
+percentage = 85.5
+
+print(height)
+print(percentage)
+print(type(height))
+#Boolean (bool)
+#Definition: A Boolean data type has only two possible values:
+#True
+#False
+is_student = True
+is_working = False
+
+print(is_student)
+print(is_working)
+print(type(is_student))
